9f9598c7c0440983a1d309e704a6e8c2e826f3f2
Shashank Awasthy <Shashank1766@users.noreply.github.com>
2016-08-14 20:32:10 +0530

    Add files via upload

---
+++ Users/UsersScript.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    //$("#UsersFormDiv").validationEngine();
+});
+
+function SaveUsers(id) {
+    //var isValid = jQuery("#UsersFormDiv").validationEngine({ returnIsValid: true });
+    
+}
+
+function EditUser(id) {
+    alert("Edit user with user id :" + id);
+}
+
+function DeleteUser(id) {
+    alert("Delete user with user id :" + id);
+}
+
+function BindUsersGrid() {
+    
+}
+
+function ClearForm() {
+    
+}
+
+function ClearAll() {
+    
+}
+
+
+
+
