a3d7bbfd80de8b12a6f7f65fa2fe3eb7d0ab1939
Shashank Awasthy <Shashank1766@users.noreply.github.com>
2016-08-14 19:51:19 +0530

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FilterConfig.cs" company="ness">
+//   Shashank
+// </copyright>
+// <summary>
+//   The filter config.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BidKaro.App_Start
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The filter config.
+    /// </summary>
+    public class FilterConfig
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The register global filters.
+        /// </summary>
+        /// <param name="filters">
+        /// The filters.
+        /// </param>
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new ElmahErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
+        }
+
+        #endregion
+    }
+}
